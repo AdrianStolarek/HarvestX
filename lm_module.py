0cf845a95b206929a728d9aa3ce7a26884472627
Adrian Stolarek <mrodziu@gmail.com>
2024-08-06 19:58:41 +0200

    Fundamental project changes. No more twitter API, now project uses selenium driver and extracts information using Xpath. Still a lot of work to do with that.

--- lm_module.py
+++ lm_module.py
@@ -1,8 +1,8 @@
 from transformers import pipeline
-import pandas as pd
+from transformers import LlamaForCausalLM, LlamaTokenizer
 
-model_name = "huggingface/llama-2"
-nlp = pipeline("text-generation", model=model_name)
+model_name = "apple/OpenELM-270M-Instruct"
+nlp = pipeline("text-generation", model=model_name, trust_remote_code=True)
 
 def analyze_text_with_local_llm(text):
     response = nlp(text, max_length=50, num_return_sequences=1)
@@ -10,5 +10,5 @@ def analyze_text_with_local_llm(text):
 
 def extract_keywords_and_summary(tweet_text):
     summary = analyze_text_with_local_llm(tweet_text)
-    keywords = summary.split()[:5]
+    keywords = summary.split()
     return keywords, summary
--- scrap_tweet.py
+++ scrap_tweet.py
@@ -1,36 +1,95 @@
 from bs4 import BeautifulSoup as bs
-import requests as req
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
 import json
-import tweepy
+import time
 from lm_module import extract_keywords_and_summary
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-API_KEY = 'YOUR_API_KEY'
-API_SECRET_KEY = 'YOUR_API_SECRET_KEY'
-ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
-ACCESS_TOKEN_SECRET = 'YOUR_ACCESS_TOKEN_SECRET'
+def configure_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    return driver
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+#def collect_tweets(query, count=100):
+#    driver = configure_driver()
+#    search_url = f"https://x.com/{query}"
+#    driver.get(search_url)
+#    time.sleep(3)
+#
+#    tweets_data = []
+#    last_height = driver.execute_script("return document.body.scrollHeight")
+#
+#    while len(tweets_data) < count:
+#        tweets = driver.find_elements(By.XPATH, '//article[@data-testid="tweet"]')
+#        for tweet in tweets:
+#            try:
+#                user = tweet.find_element(By.XPATH, './/span').text
+#                text = tweet.find_element(By.XPATH, './/div[2]/div[2]/div[1]').text
+#                created_at = tweet.find_element(By.XPATH, './/time').get_attribute('datetime')
+#                retweet_count = tweet.find_element(By.XPATH, './/div[@data-testid="retweet"]').text
+#                favorite_count = tweet.find_element(By.XPATH, './/div[@data-testid="like"]').text
+#
+#                tweet_info = {
+#                    'user': user,
+#                    'text': text,
+#                    'created_at': created_at,
+#                    'retweet_count': int(retweet_count) if retweet_count else 0,
+#                    'favorite_count': int(favorite_count) if favorite_count else 0
+#                }
+#                tweets_data.append(tweet_info)
+#
+#                if len(tweets_data) >= count:
+#                    break
+#            except Exception as e:
+#                print("Error while scraping tweet:", e)
+#                continue
+#
+#        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#        time.sleep(3)
+#        new_height = driver.execute_script("return document.body.scrollHeight")
+#        if new_height == last_height:
+#            break
+#        last_height = new_height
+#
+#    driver.quit()
+#    return tweets_data
 
 def collect_tweets(query, count=100):
+    driver = configure_driver()
+    search_url = f"https://x.com/{query}"
+    driver.get(search_url)
+    time.sleep(3)
+
     tweets_data = []
-    for tweet in tweepy.Cursor(api.search_tweets, q=query, lang="en", tweet_mode="extended").items(count):
-        tweet_info = {
-            'user': tweet.user.screen_name,
-            'text': tweet.full_text,
-            'created_at': tweet.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-            'retweet_count': tweet.retweet_count,
-            'favorite_count': tweet.favorite_count
-        }
-        tweets_data.append(tweet_info)
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while len(tweets_data) < count:
+        tweets = driver.find_elements(By.XPATH, '//*[@id="id__k4jrnoy69fd"]')
+        for tweet in tweets:
+            try:
+                print(tweet)
+                tweets_data.append(tweet)
+            except Exception as e:
+                print("Error while scraping tweet:", e)
+                continue
+
+    driver.quit()
     return tweets_data
 
-queries = ["#Python", "#DataScience", "#MachineLearning"]
+queries = ["realDonaldTrump", "elonmusk", "ElbridgeColby", "TuckerCarlson", "JDVance"]
 all_tweets = []
 
 for query in queries:
-    tweets = collect_tweets(query, count=100)
+    tweets = collect_tweets(query, count=5)
     all_tweets.extend(tweets)
 
 with open('tweets.json', 'w') as json_file:
