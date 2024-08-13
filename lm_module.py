0f9d37a9efcc5276dba68eed1274e982a64fb79f
Adrian Stolarek <mrodziu@gmail.com>
2024-08-14 00:20:07 +0200

    Major changes to the structure that is scrapped in the web + NLP fix

--- lm_module.py
+++ lm_module.py
@@ -1,14 +1,18 @@
 from transformers import pipeline
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_name = "apple/OpenELM-270M-Instruct"
-nlp = pipeline("text-generation", model=model_name, trust_remote_code=True)
+#model_name = "apple/OpenELM-270M-Instruct"
+model_name = "gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+nlp = pipeline("text-generation", model=model, tokenizer=tokenizer, trust_remote_code=True)
 
 def analyze_text_with_local_llm(text):
     response = nlp(text, max_length=50, num_return_sequences=1)
     return response[0]['generated_text']
 
 def extract_keywords_and_summary(tweet_text):
+    #Space for pushing data into df
     summary = analyze_text_with_local_llm(tweet_text)
     keywords = summary.split()
     return keywords, summary
--- scrap_tweet.py
+++ scrap_tweet.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
+from pandas import DataFrame as df
 import json
 import time
 from lm_module import extract_keywords_and_summary
@@ -19,50 +20,6 @@ def configure_driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     return driver
 
-#def collect_tweets(query, count=100):
-#    driver = configure_driver()
-#    search_url = f"https://x.com/{query}"
-#    driver.get(search_url)
-#    time.sleep(3)
-#
-#    tweets_data = []
-#    last_height = driver.execute_script("return document.body.scrollHeight")
-#
-#    while len(tweets_data) < count:
-#        tweets = driver.find_elements(By.XPATH, '//article[@data-testid="tweet"]')
-#        for tweet in tweets:
-#            try:
-#                user = tweet.find_element(By.XPATH, './/span').text
-#                text = tweet.find_element(By.XPATH, './/div[2]/div[2]/div[1]').text
-#                created_at = tweet.find_element(By.XPATH, './/time').get_attribute('datetime')
-#                retweet_count = tweet.find_element(By.XPATH, './/div[@data-testid="retweet"]').text
-#                favorite_count = tweet.find_element(By.XPATH, './/div[@data-testid="like"]').text
-#
-#                tweet_info = {
-#                    'user': user,
-#                    'text': text,
-#                    'created_at': created_at,
-#                    'retweet_count': int(retweet_count) if retweet_count else 0,
-#                    'favorite_count': int(favorite_count) if favorite_count else 0
-#                }
-#                tweets_data.append(tweet_info)
-#
-#                if len(tweets_data) >= count:
-#                    break
-#            except Exception as e:
-#                print("Error while scraping tweet:", e)
-#                continue
-#
-#        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#        time.sleep(3)
-#        new_height = driver.execute_script("return document.body.scrollHeight")
-#        if new_height == last_height:
-#            break
-#        last_height = new_height
-#
-#    driver.quit()
-#    return tweets_data
-
 def collect_tweets(query, count=100):
     driver = configure_driver()
     search_url = f"https://x.com/{query}"
@@ -73,11 +30,14 @@ def collect_tweets(query, count=100):
     last_height = driver.execute_script("return document.body.scrollHeight")
 
     while len(tweets_data) < count:
-        tweets = driver.find_elements(By.XPATH, '//*[@id="id__k4jrnoy69fd"]')
+        tweets = driver.find_elements(By.XPATH, '//article[@role="article"]//div[@data-testid="tweetText"]')
         for tweet in tweets:
             try:
-                print(tweet)
-                tweets_data.append(tweet)
+                text = tweet.text
+                print(text)
+                tweets_data.append({'text': text})
+                if len(tweets_data) >= count:
+                    break
             except Exception as e:
                 print("Error while scraping tweet:", e)
                 continue
@@ -92,8 +52,8 @@ for query in queries:
     tweets = collect_tweets(query, count=5)
     all_tweets.extend(tweets)
 
-with open('tweets.json', 'w') as json_file:
-    json.dump(all_tweets, json_file, indent=4)
+json_data = json.dumps(all_tweets, indent=4)
+print(json_data)
 
 analyzed_tweets = []
 for tweet in all_tweets:
@@ -106,5 +66,5 @@ for tweet in all_tweets:
     }
     analyzed_tweets.append(analyzed_tweet)
 
-with open('analyzed_tweets.json', 'w') as file:
-    json.dump(analyzed_tweets, file, indent=4)
+json_analyzed_data = json.dumps(analyzed_tweets, indent=4)
+print(json_analyzed_data)
